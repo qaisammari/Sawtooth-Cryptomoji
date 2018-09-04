a4b3bd06f53b2417e9c8f5bbed16b46d8196441f
Qais Ammari <qais.ammari@triphop.com>
2018-09-04 12:37:40 +0300

    addressing

--- client/source/services/addressing.js
+++ client/source/services/addressing.js
@@ -3,11 +3,12 @@ import { createHash } from 'crypto';
 
 const NAMESPACE = '5f4d76';
 const PREFIXES = {
-  COLLECTION: '00',
-  MOJI: '01',
-  SIRE_LISTING: '02',
-  OFFER: '03'
+    COLLECTION: '00',
+    MOJI: '01',
+    SIRE_LISTING: '02',
+    OFFER: '03'
 };
+const hash = str => createHash('sha512').update(str).digest('hex');
 /**
  * A function which optionally takes a public key, and returns a full or
  * partial collection address.
@@ -25,8 +26,14 @@ const PREFIXES = {
  *   // '5f4d7600ecd7ef459ec82a01211983551c3ed82169ca5fa0703ec98e17f9b534ffb797'
  */
 export const getCollectionAddress = (publicKey = null) => {
-  // Enter your solution here
+    // Enter your solution here
+    if (publicKey===null) {
+        return NAMESPACE+PREFIXES.COLLECTION;
+    }else{
+        console.log(typeof publicKey, " Publickey: ",publicKey,publicKey.length);
 
+        return NAMESPACE+PREFIXES.COLLECTION+hash(publicKey).slice(0,62);
+    }
 };
 
 /**
@@ -42,8 +49,22 @@ export const getCollectionAddress = (publicKey = null) => {
  *   console.log(ownerPrefix);  // '5f4d7601ecd7ef45'
  */
 export const getMojiAddress = (ownerKey = null, dna = null) => {
-  // Your code here
+    // Your code here
+    if ( ownerKey===null && dna===null) {
 
+        return NAMESPACE+PREFIXES.MOJI
+    }
+    else if (ownerKey===null) {
+        return NAMESPACE+PREFIXES.COLLECTION;
+    }else if(dna === null) {
+
+        return NAMESPACE+PREFIXES.MOJI+hash(ownerKey).slice(0,8);
+
+    }else {
+
+        console.log(typeof dna, " dna: ",dna,dna.length);
+        return NAMESPACE+PREFIXES.MOJI+hash(ownerKey).slice(0,8)+hash(dna).slice(0,54);
+    }
 };
 
 /**
@@ -54,7 +75,12 @@ export const getMojiAddress = (ownerKey = null, dna = null) => {
  * otherwise returns the full address.
  */
 export const getSireAddress = (ownerKey = null) => {
-  // Your code here
+    // Your code here
+    if (ownerKey===null) {
+        return NAMESPACE+PREFIXES.SIRE_LISTING;
+    }else{
+        return NAMESPACE+PREFIXES.SIRE_LISTING+hash(ownerKey).slice(0,62);
+    }
 
 };
 
@@ -71,6 +97,11 @@ export const getSireAddress = (ownerKey = null) => {
  * The identifiers may be either moji dna, or moji addresses.
  */
 export const getOfferAddress = (ownerKey = null, moji = null) => {
-  // Your code here
+    // Your code here
 
 };
+
+
+//5f4d76 01 8ebde14 c6d2a201aa6e9e5b5fa7e96d22f9eb698e483bf4f126bdd1a2b9900
+
+//5f4d76 01 8ebde14 ca6e9e5b5fa7e96d22f9eb698e483bf4f126bdd1a2b9900fe4c5c1d
