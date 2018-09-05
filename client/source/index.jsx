69f99102f9889a29e1bd8d814df816f4c0121c9b
Qais Ammari <qais.ammari@triphop.com>
2018-09-05 14:28:14 +0300

    fixed the http request

--- client/source/index.jsx
+++ client/source/index.jsx
@@ -12,35 +12,72 @@ const publickKey = keys.publicKey;
 
 console.log("WE GOT PRIVATE KEY: ", privateKey);
 
+let webMessage = "";
+let addressList = [];
+
+
+// axios({
+//     method: 'post',
+//     url: '/api/batches',
+//     data: encodeAll(privateKey, {action: "CREATE_COLLECTION"}),
+//     headers: {'Content-Type' : 'application/octet-stream'}
+// }).then(function(response) {
+//     console.log("RESPONSE: ", response);
+// }).catch(function (error) {
+//     console.log("ERROR: ", error);
+// });
+
+webMessage = "Getting Mojies";
+
 
 axios({
-    method: 'post',
-    url: '/api/batches',
-    data: encodeAll(privateKey, {action: "CREATE_COLLECTION"}),
-    headers: {'Content-Type' : 'application/octet-stream'}
+    method: 'get',
+    url: '/api/state'
 }).then(function(response) {
+    webMessage = "We got the mojies ... ";
     console.log("RESPONSE: ", response);
-}).catch(function (error) {
+    addressList =  response.data.data.map( d => {
+        return (
+            <li>{d.address}</li>
+        )
+    });
+}).then(function() {
+    console.log("DATA LIST: ", addressList.length);
+}).then(function()  {
+    ReactDOM.render((
+        <BrowserRouter>
+            <div>
+                <h1>Hello, Cryptomoji!</h1>
+                <h3>{webMessage}</h3>
+                <h2>Keys:</h2>
+                <ul>
+                    <li>
+                        {privateKey}
+                    </li>
+                    <li>
+                        {publickKey}
+                    </li>
+                </ul>
+
+
+                <h1>Mojies List</h1>
+                <ul>
+                    {addressList}
+                </ul>
+            </div>
+        </BrowserRouter>
+    ), document.getElementById('app'));
+})
+
+
+    .catch(function (error) {
     console.log("ERROR: ", error);
+    webMessage = "Failed to get the mojies ... "
+
 });
 
 
 
 
-ReactDOM.render((
-  <BrowserRouter>
-    <div>
-        <h1>Hello, Cryptomoji!</h1>
-        <h2>Keys:</h2>
-        <ul>
-            <li>
-                {privateKey}
-            </li>
-            <li>
-                {publickKey}
-            </li>
-        </ul>
-    </div>
-  </BrowserRouter>
-), document.getElementById('app'));
+
 
