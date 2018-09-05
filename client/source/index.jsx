4edea23eee3e817a4ac3564ed8ee85f46d1818fe
Qais Ammari <qais.ammari@triphop.com>
2018-09-05 11:20:50 +0300

    created the first http request on the client

--- client/source/index.jsx
+++ client/source/index.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import {createKeys} from "./services/signing";
+import {encodeAll} from "./services/transactions";
+
+const keys = createKeys();
+const privateKey = keys.privateKey;
+
+
+console.log("WE GOT PRIVATE KEY: ", privateKey);
+
+
+axios({
+    method: 'post',
+    url: 'localhost:8008/rest/batches',
+    data: encodeAll(privateKey, {action: "CREATE_COLLECTION"}),
+    headers: {'Content-Type' : 'application/octet-stream'}
+}).then(function(response) {
+    console.log("RESPONSE: ", response);
+}).catch(function (error) {
+    console.log("ERROR: ", error);
+});
+
+
+
+
+
+
+
 
 
 ReactDOM.render((
